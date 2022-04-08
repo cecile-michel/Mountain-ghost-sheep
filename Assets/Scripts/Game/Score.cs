cb661e77ffef6971bb5c17ae7f9a45e2fd1ea00f
Cocograal <willi1maril@gmail.com>
2022-04-08 20:47:30 +0200

    display score ajouté, problème de vitesse à régler

--- Assets/Scripts/Game/Score.cs
+++ Assets/Scripts/Game/Score.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Score : MonoBehaviour {
 
     public int score;
+    public Text text;
+    public enum Dog {
+        dog1 = 0,
+        dog2 = 1,
+        null_ = 2
+    }
+    public Dog dog;
     
+    public void Start() {
+        displayScore();
+    }
+
     public void addScore(){
         score = score + 1;
+        displayScore();
     }
     
     public void removeScore(){
         score = score - 1;
-        print("MMMMMMMM");
+        displayScore();
 
+    }
+
+    public void displayScore() {
+        if (dog == 0) {
+            text.text = "Score joueur 1: " + score;
+        } else {
+            text.text = "Score joueur 2: " + score;
+        }
     }
 }
