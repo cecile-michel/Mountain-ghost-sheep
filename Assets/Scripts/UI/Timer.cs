94922b7b4e567296ccfc18f5deec57dafaabc079
Cocograal <willi1maril@gmail.com>
2022-04-30 17:27:02 +0200

    change of the length of the game added in the settings of the menu

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -10,16 +10,20 @@ public class Timer : MonoBehaviour
     private float initTimerValue;
     private Text timerText;
     public Slider slider;
-    public float maxMinutes = 2;
+    private float maxMinutes = 2;
     public GameManager gameManager;
 
     private float timeLeft;
+
+    private TimeManager TimeManager;
+
     public void Awake() {
         initTimerValue = Time.time; 
     }
 
     // Start is called before the first frame update
     public void Start() {
+        maxMinutes = TimeManager.time;
         timerText = GetComponent<Text>();
         timerText.text = "temps restant " + string.Format("{0:00}:{1:00}", 0, 0);
         timeLeft = maxMinutes*60;
